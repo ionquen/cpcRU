b4a4d6ae97c0926e5dc00a403195e84290a1ec4f
ionquen <ionqwen@gmail.com>
2022-03-28 12:19:31 +0300

    upd

---
+++ global.js
@@ -0,0 +1,21 @@
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-XZD2FJ1C58', {
+  page_path: window.location.pathname,
+});
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ym(84896236, "init", {
+    clickmap:true,
+    trackLinks:true,
+    accurateTrackBounce:true,
+    webvisor:true
+});
+function sendMetrik(event) {
+    gtag('event', event);
+    ym(84896236,'reachGoal',event);
+}
+window.addEventListener('appinstalled', () => sendMetrik('installed'));
